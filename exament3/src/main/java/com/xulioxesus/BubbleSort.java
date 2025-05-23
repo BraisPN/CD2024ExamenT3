6936806008bc1e45cb07345006de783d24b56952
Brais Pose Nieto <braispose15@gmail.com>
2025-05-23 10:50:27 +0200

    Actualizar BubbleSort.java

--- exament3/src/main/java/com/xulioxesus/BubbleSort.java
+++ exament3/src/main/java/com/xulioxesus/BubbleSort.java
@@ -1,19 +1,27 @@
 package com.xulioxesus;
 
+/**
+ * Clase en la que tenemos un metodo de ordenacion de burbuja.
+ */
 public class BubbleSort {
 
-    public void ordenar(int[] arr, int n) {
-        for (int i = 0; i < n - 1; i++) {
-            boolean swapped = false;
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    swapped = true;
+    /**
+     * Este método ordena una lista de números.
+     * @param listaNumeros Lista de números a ordenar
+     * @param cantidade Cantidad de números de la lista
+     */
+    public void ordenar(int[] listaNumeros, int cantidade) {
+        for (int i = 0; i < cantidade - 1; i++) {
+            boolean intercambiado = false;
+            for (int j = 0; j < cantidade - i - 1; j++) {
+                if (listaNumeros[j] > listaNumeros[j + 1]) {
+                    int auxiliar = listaNumeros[j];
+                    listaNumeros[j] = listaNumeros[j + 1];
+                    listaNumeros[j + 1] = auxiliar;
+                    intercambiado = true;
                 }
             }
-            if (!swapped) {
+            if (!intercambiado) {
                 break;
             }
         }
