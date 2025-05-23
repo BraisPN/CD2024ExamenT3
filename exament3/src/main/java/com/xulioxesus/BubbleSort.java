a35546b041ed3017fc6a433d2e4c070f049c4066
Brais Pose Nieto <braispose15@gmail.com>
2025-05-23 09:37:19 +0200

    Actualizar BubbleSort.java

--- exament3/src/main/java/com/xulioxesus/BubbleSort.java
+++ exament3/src/main/java/com/xulioxesus/BubbleSort.java
@@ -1,25 +1,21 @@
 package com.xulioxesus;
 
-class BubbleSort{
-    
-    public void ordenar(int [] arr, int n)
-    {
-        int i, j, temp;
-        boolean swapped;
-        for (i = 0; i < n - 1; i++) {
-            swapped = false;
-            for (j = 0; j < n - i - 1; j++) {
+public class BubbleSort {
+
+    public void ordenar(int[] arr, int n) {
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    
-                    temp = arr[j];
+                    int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                     swapped = true;
                 }
             }
-
-            if (swapped == false)
+            if (!swapped) {
                 break;
+            }
         }
     }
 }
